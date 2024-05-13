f50eac931f26aed26871bb0cefe3783d535fdcdc
TritonDeNaranja <49379486+TritonDeNaranja@users.noreply.github.com>
2024-05-13 15:56:04 -0400

    feat: add getAll method and UpdateStock method

--- ebooks-dotnet7-api/Program.cs
+++ ebooks-dotnet7-api/Program.cs
@@ -7,16 +7,17 @@ builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("ebook
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
-var ebooks = app.MapGroup("api/ebook");
+var ebooks = app.MapGroup("api");
 
 // TODO: Add more routes
 ebooks.MapPost("/ebook", CreateEBookAsync);
-ebooks.MapGet("/ebook?genre={genre}&author={author}&formtat={format}",GetAllAvailable);
-ebooks.MapPut("/ebook/{id}",UpdateBook);
-ebooks.MapPut("/ebook/{id}/change-availability)",UpdateAvailability);
-ebooks.MapPut("/ebook/{id}/increment-stock",IncreaseStock);
-ebooks.MapPost("/ebook/purchase",PurchaseEBook);
-ebooks.MapDelete("/ebook/{id}",DeleteEBook);
+ebooks.MapGet("/ebook/getAll",GetAllAvailable);
+//ebooks.MapPut("/ebook/{id}",UpdateBook);
+//ebooks.MapPut("/ebook/{id}/change-availability)",UpdateAvailability);
+ebooks.MapPut("/ebook/{id}/{increment-stock}",IncreaseStock);
+//ebooks.MapPost("/ebook/purchase",PurchaseEBook);
+
+ebooks.MapDelete("/ebook/{id}",DeleteEBook); 
 
 
 app.Run();
@@ -26,39 +27,58 @@ async Task<IResult> CreateEBookAsync(EBook ebook, DataContext db)
 {
     db.EBooks.Add(ebook);
     await db.SaveChangesAsync();
-    return TypedResults.Created($"/todoitems/{ebook.Id}", ebook);
+    return TypedResults.Created($"/ebooks/{ebook.Id}", ebook);
 
 }
 
 async Task<IResult> GetAllAvailable(DataContext db)
 {
+    
     return TypedResults.Ok(await db.EBooks.Where(t => t.IsAvailable).ToListAsync());
 }
 
 
 
-async Task UpdateBook(HttpContext context)
+/* async Task<IResult> UpdateBook(DataContext db)
 {
     throw new NotImplementedException();
+} */
+
+/* async Task UpdateAvailability(HttpContext context)
+{
+    throw new NotImplementedException();
+}
+*/
+
+async Task<IResult> IncreaseStock(DataContext db, int id, EBook inputEbook)
+{
+    var ebook = await db.EBooks.FindAsync(id);
+    if (ebook is null) return TypedResults.NotFound();
+    /*ebook.Title = inputEbook.Title;
+    ebook.Author = inputEbook.Author;
+    ebook.Genre = inputEbook.Genre;
+    ebook.Format = inputEbook.Format;
+    ebook.IsAvailable = inputEbook.IsAvailable;
+    ebook.Price = inputEbook.Price; */
+    ebook.Stock = inputEbook.Stock;
+    await db.SaveChangesAsync();
+    return TypedResults.NoContent();
 }
 
-async Task UpdateAvailability(HttpContext context)
+/* async Task PurchaseEBook(HttpContext context)
 {
     throw new NotImplementedException();
-}
-
-async Task IncreaseStock(HttpContext context)
+} */
+ 
+async  Task<IResult> DeleteEBook(DataContext db, int id)
 {
-    throw new NotImplementedException();
-}
-
-async Task PurchaseEBook(HttpContext context)
-{
-    throw new NotImplementedException();
-}
-
-async Task DeleteEBook(HttpContext context)
-{
-    throw new NotImplementedException();
-}
+    
+    if (await db.EBooks.FindAsync(id) is EBook ebook)
+    {
+        db.EBooks.Remove(ebook);
+        await db.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+    return TypedResults.NotFound();
+} 
 
